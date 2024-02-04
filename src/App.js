29cd9bd4d5b0ec1a5bc5a5077bef0ae2851e6e90
apterousteen <ayzhan312@live.ru>
2024-02-04 14:50:02 +0500

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles/App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>hello there</p>
+      </header>
+    </div>
+  );
+}
